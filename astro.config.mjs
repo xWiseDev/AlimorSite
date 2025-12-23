572a928e5f3ca8bb0e54e8a5a4207ca161559f19
xWiseDev <aboyahyadev@outlook.com>
2025-12-23 16:52:30 +0100

    Update paths for custom domain alimor.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://xWiseDev.github.io',
-  base: '/AlimorSite',
+  site: 'https://alimor.app',
+  base: '/',
   integrations: [tailwind()],
   build: {
     assets: '_assets'
